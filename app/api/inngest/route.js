9636d15cbe0984be9b6c12ca7c256bbcc6d9627a
Ganesh Shinde <ganeshshinde0100@gmail.com>
2025-12-28 18:40:58 +0530

    .

--- app/api/inngest/route.js
+++ app/api/inngest/route.js
@@ -3,8 +3,8 @@ import { serve } from "inngest/next";
 import { inngest } from "@/lib/inngest/client";
 import { generateIndustryInsights } from "@/lib/inngest/function";
 
-// Configure this route to be dynamic to avoid static export issues
-export const dynamic = 'force-dynamic';
+// Configure this API route for server-side execution
+export const runtime = 'nodejs';
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
